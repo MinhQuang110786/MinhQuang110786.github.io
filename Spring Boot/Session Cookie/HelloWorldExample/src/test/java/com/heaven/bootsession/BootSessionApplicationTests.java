2a86297c325de8369f7c0d5ab9135fcd5ccbea87
heavenlight86@gmail.com <heavenlight86@gmail.com>
2021-01-14 11:39:49 +0700

    Update Spring Boot

---
+++ Spring Boot/Session Cookie/HelloWorldExample/src/test/java/com/heaven/bootsession/BootSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heaven.bootsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
